81094ae8a832898d8c6c3e74a2f93638f190d4bb
MakoMR <mak.gumpf@gmail.com>
2026-04-19 19:18:51 -0400

    created a register through the fastapi that will create a new user.

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,5 @@
 from passlib.hash import bcrypt
 
 def hash_password(password):
-    e_pass = bcrypt.hash(password)
-    return e_pass
+    hashed_pass = bcrypt.hash(password)
+    return hashed_pass
--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
-from database import base, engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from schemas import UserCreate
+from database import base, engine, get_db
+from auth import hash_password
 import models
 
 app = FastAPI()
@@ -8,3 +11,15 @@ base.metadata.create_all(engine)
 @app.get("/")
 def root():
     return {"message": "Portfolio API is running"}
+
+@app.post("/register")
+def create_register(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_pass = hash_password(user.password)
+    new_user = models.User(email=user.email, password=hashed_pass)
+    db.add(new_user)
+    db.commit()
+    return {"message": "User created successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
