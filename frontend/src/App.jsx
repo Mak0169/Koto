a3a96f160e08ec8f96642afd03a096b0b35ab0e9
MakoMR <mak.gumpf@gmail.com>
2026-04-29 23:00:11 -0400

    Adding some of the frontend stuff

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Login'
+
+function App() {
+  return (
+    <div>
+      <h1>Koto</h1>
+      <SignUpBotton />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login
