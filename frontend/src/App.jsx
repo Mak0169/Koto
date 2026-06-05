51a20ca933ba79a2eb91bf8c5a9ba7bee67a0434
MakoMR <mak.gumpf@gmail.com>
2026-06-04 22:53:28 -0400

    Added a deck file that grabs the decks after login.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Signup from './Signup'
+import Deck from './Deck'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/deck" element={<Deck />}/>
       </Routes>
     </BrowserRouter> 
   )
